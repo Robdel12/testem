47f307fab181d1ad8478beb084c493e159a8f3c0
Robert DeLuca <robertd@izea.com>
2015-02-17 17:55:22 -0500

    Allow passing of user agent string

--- assets/phantom.js
+++ assets/phantom.js
@@ -1,8 +1,12 @@
 var system = require('system');
 var page = require('webpage').create();
 var url = system.args[1];
+var userAgent = system.args[2];
 page.viewportSize = {
   width: 1024,
   height: 768
 };
+if(userAgent !== 'false'){
+  page.settings.userAgent = userAgent;
+}
 page.open(url);
